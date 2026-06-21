4903bbce6eb0edfcf7b70b8ae2942e998cbd8cfd
Lounéa <tommygms10@gmail.com>
2026-06-21 13:41:37 +0200

    Delete components directory

--- components/layout/SmoothScrollProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useLenis } from "@/hooks/useLenis";
-
-export default function SmoothScrollProvider({ children }: { children: React.ReactNode }) {
-  useLenis();
-  return <>{children}</>;
-}
